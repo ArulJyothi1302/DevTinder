fe0fabfcbb31afe5cf693ec89b30c09f3039ab80
Arul <hariarul2002@gmail.com>
2024-12-22 14:30:59 +0530

    dbCon and Db Schema Validation

--- src/app.js
+++ src/app.js
@@ -1,52 +1,96 @@
 const express = require("express");
 const app = express();
-const { AdminAuth, userAuth } = require("./middleware/auth");
+const conDb = require("./config/database");
+app.use(express.json());
+const User = require("./models/user");
 
-//Middleware
-{
-  app.post("/user/login", (req, res) => {
-    //for login page Authorization not needed
-    res.send("Enter Credentials");
-  });
-  app.get("/user", userAuth, (req, res) => {
-    //single route so  authorization will check here if true come to res else userAuth will send status error
-    res.send("User Acessed");
-  });
-  app.use("/admin", AdminAuth);
-  app.get("/admin/getProfile", (req, res, next) => {
-    res.send("Profile Viewed");
-  });
-  app.get("/admin/deleteProfile", (req, res) => {
-    res.send("Deleted Successfully");
-  });
-}
-
-//ERROR HANDLING
-// if I used here and calling/getdata getdata only will call and throw error coz i used try catch
-//if there is no common route with error handling and try catch in /getdata it will return the error warning
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("Something Went Wrong");
-  }
-});
-app.use("/getdata", (req, res) => {
-  // throw new Error("error happened");
-  // res.send("Data got");
-  // use try catch for better error handling
+app.post("/signup", async (req, res) => {
+  const user = new User(
+    req.body
+    //   {
+    //   fName: "Arul",
+    //   lName: "Jyothi",
+    //   email: "arul@gmail.com",
+    //   password: "arul1302",
+    // }
+  );
   try {
-    throw new Error("error happened");
-    res.send("Data Got");
+    user.save();
+    res.send("Data Added successfully");
   } catch (err) {
-    res.status(500).send("Something went wrong Contact Customer Support");
-  }
-  // if no try catch below code will execute
-});
-
-// to handle this
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("Something Went Wrong");
+    res.status(404).send("Error in saving " + err.message);
   }
 });
 
-app.listen(7777);
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  const user = await User.findOne({ email: userEmail });
+  if (!user) {
+    res.status(404).send("User not found");
+  } else {
+    res.send(user);
+  }
+});
+app.get("/user1", async (req, res) => {
+  const userId = req.body.id;
+  const user = await User.findById(userId);
+  if (!user) {
+    res.status(404).send("User not found");
+  } else {
+    res.send(user);
+  }
+});
+//Delete
+app.delete("/user", async (req, res) => {
+  const userId = req.body.id;
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+    user
+      ? res.send("Deleted Succefully")
+      : res.status(404).send("No user Found ");
+  } catch (err) {
+    res.status(404).send("Something went Wrong");
+  }
+});
+
+//Updating one record
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.id;
+  const userEmail = req.body.email;
+  const upData = req.body;
+  // try {
+  //   const user = await User.findByIdAndUpdate(userId, upData, {
+  //     returnDocument: "after",
+  //   });
+  //   console.log(user);
+  //   res.send("Updated Succefully");
+  // }
+  try {
+    const user = await User.findOneAndUpdate({ email: userEmail }, upData, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    console.log(user);
+    res.send("Updated Succefully");
+  } catch (err) {
+    res.status(404).send("Something Went Wrong");
+  }
+});
+
+//Get all data
+app.get("/feed", async (req, res) => {
+  const user = await User.find({});
+
+  user.length === 0 ? res.send(user) : res.status(404).send("User Not Found");
+});
+conDb()
+  .then(() => {
+    console.log("Db Connected Succefully");
+    app.listen(7777, () => {
+      console.log("Server connected to port 7777");
+    });
+  })
+  .catch((err) => {
+    console.error("Not Connected");
+  });
